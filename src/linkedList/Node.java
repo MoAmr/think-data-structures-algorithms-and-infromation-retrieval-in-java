1f60b2038409f0f1418e6f80795f5935b48c9530
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-02-20 21:57:50 +0200

    Implementing Node class of LinkedList Pacakge

---
+++ src/linkedList/Node.java
@@ -0,0 +1,32 @@
+package linkedList;
+
+/**
+ * @author Mohammed Amr
+ * @created 20/02/2021 - 21:54
+ * @project think-data-structures
+ */
+public class Node {
+
+    private int data;
+    private Node nextNode;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+}
